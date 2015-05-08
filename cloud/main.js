393bc144eb1f4eca53579ec9b0b5c3de6f64dfdf
Angelo Ashmore <angeloashmore@me.com>
2015-05-08 11:12:46 -1000

    Rewriting User__create

--- cloud/main.js
+++ cloud/main.js
@@ -1,37 +1,38 @@
 var Stripe = require('stripe');
 Stripe.initialize('sk_test_QSGWG4k2CfgfD10hXdWVdOXc');
 
-Parse.Cloud.define("createCustomer", function(request, response) {
+Parse.Cloud.define("User__create", function(request, response) {
   Parse.Promise.as().then(function() {
-    // Find the plan.
-    var planQuery = new Parse.Query('Plan');
-    planQuery.equalTo('name', request.params.planName);
+    var user = new Parse.User();
+    user.set("username", request.params.username);
+    user.set("password", request.params.password);
+    user.set("email", request.params.email);
 
-    // Find the results.
-    return planQuery.first().then(null, function(error) {
-      return Parse.Promise.error('Error: Plan could not be found');
-    });
+    return user.signUp()
+      .fail(function(error) {
+        return Parse.Promise.error('User could not be created. Error: ' + error.message);
+      });
 
-  }).then(function(result) {
-    // Make sure we found a plan.
-    if (!result) {
-      return Parse.Promise.error('Error: Plan could not be found');
-    }
+  }).then(function(user) {
+    return stripeCustomer = Stripe.Customers.create({
+      email: user.attributes.email,
+      metadata: {
+        parseUserId: user.id
+      }
+    }).then(function(stripeCustomer) {
+      return { user: user, stripeCustomer: stripeCustomer };
 
-    return result;
-
-  }).then(function(result) {
-    return Stripe.Customers.create({
-      email: request.params.email,
-      source: request.params.token,
-      plan: result.stripeId
-    }).then(null, function(error) {
+    }).fail(function(error) {
       console.log("Creating the customer with Stripe failed. Error: " + error);
-      return Parse.Promise.error('An error has occured. Your credit card was not charged.');
+      return Parse.Promise.error('An error has occured.');
     });
 
-  }).then(function(customer) {
-    response.success('Success!');
+  }).then(function(results) {
+    var user = results.user;
+    user.set("stripeCustomerId", results.stripeCustomer.id);
+    user.save().then(function(user) {
+      response.success(user);
+    });
 
   }, function(error) {
     response.error(error);
