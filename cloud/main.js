e95fabfde5a1736c3de78a8cebd5981260018e17
Angelo Ashmore <angeloashmore@me.com>
2015-05-13 12:54:41 -1000

    Adding plan selection functionality

--- cloud/main.js
+++ cloud/main.js
@@ -116,3 +116,62 @@ Parse.Cloud.define("User__addCardToken", function(request, response) {
     response.error(error);
   });
 });
+
+Parse.Cloud.define("User__subscribeTo", function(request, response) {
+  var user = request.user;
+  var plan, billing;
+
+  Parse.Promise.as().then(function() {
+    // Validate user and params.
+    if (!user) {
+      return Parse.Promise.error("User is not logged in");
+    }
+
+    if (!request.params.planId) {
+      return Parse.Promise.error("Plan ID was not provided");
+    }
+
+  }).then(function() {
+    var Plan = Parse.Object.extend("Plan");
+    var query = new Parse.Query(Plan);
+    return query.get(request.params.planId)
+      .fail(function(error) {
+        return Parse.Promise.error("Plan could not be found. Error:" + error);
+      });
+
+  }).then(function(_plan) {
+    plan = _plan;
+
+    return request.user.get("billing").fetch()
+      .fail(function(error) {
+        return Parse.Promise.error("Billing info could not be found. Error:" + error);
+      });
+
+  }).then(function(_billing) {
+    billing = _billing;
+
+    var stripeCustomerId = billing.get("stripeCustomerId");
+    var data = {
+      plan: plan.get("stripePlanId")
+    };
+
+    return Stripe.Customers.updateSubscription(stripeCustomerId, data)
+      .fail(function(error) {
+        return Parse.Promise.error("Stripe Customer could not be updated. Error:" + error);
+      });
+
+  }).then(function(subscription) {
+    user.set("plan", plan);
+
+    return user.save()
+      .fail(function(error) {
+        return Parse.Promise.error("User could not be updated. Error:" + error);
+      });
+
+  }).then(function(user) {
+    response.success(user);
+
+  }, function(error) {
+    response.error(error);
+  });
+});
