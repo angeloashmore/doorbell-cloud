7ee5afe2fc25a9bd1d92e08afb4d621da69e420d
Angelo Ashmore <angeloashmore@me.com>
2015-06-07 20:57:33 -1000

    Adding fullName to User._afterSave

--- cloud/classes/User/_afterSave.js
+++ cloud/classes/User/_afterSave.js
@@ -1,8 +1,27 @@
 const Stripe = require('cloud/ext_modules').Stripe;
 
 Parse.Cloud.afterSave(Parse.User, function(request) {
-  if (!request.object.existed()) createBilling(request.object);
+  var user = request.object;
+
+  if (!request.object.existed()) createBilling(user);
+
+  generateFullName(user);
 });
+
+function generateFullName(user) {
+  Parse.Cloud.useMasterKey();
+
+  Parse.Promise.as().then(function() {
+    var fullName = user.get("firstName") + " " + user.get("lastName");
+    user.set("fullName", fullName);
+    user.save()
+      .fail(function(error) {
+        return Parse.Promise.error("Saving fullName failed. Error: " + error);
+      });
+  }, function(error) {
+    console.error(error);
+  });
+}
 
 function createBilling(user) {
   Parse.Cloud.useMasterKey();
@@ -26,7 +45,7 @@ function createBilling(user) {
     user.save()
       .fail(function(error) {
         return Parse.Promise.error("User could not be updated. Error: " + error.message);
-      })
+      });
 
   }, function(error) {
     console.error(error);
