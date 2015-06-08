3bce19fcadcc9b3c64beff2b6946fc86065c4338
Angelo Ashmore <angeloashmore@me.com>
2015-06-07 21:05:01 -1000

    Adding User._beforeSave default values from User._afterSave

--- cloud/classes/User/_afterSave.js
+++ cloud/classes/User/_afterSave.js
@@ -3,25 +3,8 @@ const Stripe = require('cloud/ext_modules').Stripe;
 Parse.Cloud.afterSave(Parse.User, function(request) {
   var user = request.object;
 
-  if (!request.object.existed()) createBilling(user);
-
-  generateFullName(user);
+  if (!user.existed()) createBilling(user);
 });
-
-function generateFullName(user) {
-  Parse.Cloud.useMasterKey();
-
-  Parse.Promise.as().then(function() {
-    var fullName = user.get("firstName") + " " + user.get("lastName");
-    user.set("fullName", fullName);
-    user.save()
-      .fail(function(error) {
-        return Parse.Promise.error("Saving fullName failed. Error: " + error);
-      });
-  }, function(error) {
-    console.error(error);
-  });
-}
 
 function createBilling(user) {
   Parse.Cloud.useMasterKey();
--- cloud/classes/User/_beforeSave.js
+++ cloud/classes/User/_beforeSave.js
@@ -1,14 +1,25 @@
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
-  if (!request.object.get("email")) {
+  var user = request.object;
+
+  if (!user.get("email")) {
     return response.error("email is required");
   }
 
-  if (!request.object.get("firstName")) {
+  if (!user.get("firstName")) {
     return response.error("firstName is required");
   }
 
-  if (!request.object.get("lastName")) {
+  if (!user.get("lastName")) {
     return response.error("lastName is required");
+  }
+
+  // Set fullName
+  var fullName = user.get("firstName") + " " + user.get("lastName");
+  user.set("fullName", fullName)
+
+  // Set default private
+  if (!user.get("private")) {
+    user.set("private", false)
   }
 
   response.success();
