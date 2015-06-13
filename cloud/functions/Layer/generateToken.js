511c664f33f837d0e122e70213caeb13f0fffc50
Angelo Ashmore <angeloashmore@me.com>
2015-06-12 17:27:11 -1000

    Namespacing Layer__generateToken function

--- cloud/functions/Layer/generateToken.js
+++ cloud/functions/Layer/generateToken.js
@@ -5,7 +5,7 @@ const privateKey = fs.readFileSync("cloud/lib/layer-parse-module/keys/layer-key.
 
 layer.initialize(config.LAYER_PROVIDER_ID, config.LAYER_KEY_ID, privateKey);
 
-Parse.Cloud.define("generateToken", function(request, response) {
+Parse.Cloud.define("Layer__generateToken", function(request, response) {
   var userID = request.params.userID;
   var nonce = request.params.nonce;
   if (!userID) throw new Error("Missing userID parameter");
