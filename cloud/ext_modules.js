800819acefce959fdb3a9205dfa35af95e30076f
Angelo Ashmore <angeloashmore@me.com>
2015-06-12 17:04:10 -1000

    Removing unnecessary duplicated function

--- cloud/ext_modules.js
+++ cloud/ext_modules.js
@@ -3,18 +3,3 @@ const config = require('cloud/config');
 
 Stripe.initialize(config.STRIPE_PUBLISHABLE_KEY);
 exports.Stripe = Stripe;
-
-exports.validateRequiredColumns = function(object, requiredColumns) {
-  for (let columnName in requiredColumns) {
-    if (!object.get(columnName)) {
-      if (requiredColumns[columnName]) {
-        user.set(columnName, requiredColumns[columnName]);
-      } else {
-        var errorMessage = columnName + " is required";
-        return Parse.Error(Parse.Error.VALIDATION_ERROR, errorMessage);
-      }
-    }
-  }
-
-  return true
-}
