69005dff511839fe596a62d83a9fb1076da7bb92
Angelo Ashmore <angeloashmore@me.com>
2015-06-17 13:47:55 -1000

    Fixing validateRequiredColumns()

--- cloud/lib/validateRequiredColumns.js
+++ cloud/lib/validateRequiredColumns.js
@@ -2,8 +2,8 @@ const validateRequiredColumns = function(object, requiredColumns) {
   return Parse.Promise.as().then(function() {
     for (var columnName in requiredColumns) {
       if (!object.get(columnName)) {
-        if (requiredColumns[columnName]) {
-          user.set(columnName, requiredColumns[columnName]);
+        if (requiredColumns[columnName] !== undefined) {
+          object.set(columnName, requiredColumns[columnName]);
         } else {
           const errorMessage = columnName + " is required";
           return Parse.Promise.error(errorMessage);
