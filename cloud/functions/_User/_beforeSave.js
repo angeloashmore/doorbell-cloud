d350e6b0e224dceed150be012e28991567874e18
Angelo Ashmore <angeloashmore@me.com>
2015-06-29 02:29:08 -1000

    Replace username with email

--- cloud/functions/_User/_beforeSave.js
+++ cloud/functions/_User/_beforeSave.js
@@ -3,5 +3,7 @@ const User = require("cloud/classes/_User");
 Parse.Cloud.beforeSave(User, function(request, response) {
   const user = request.object;
 
+  user.set("username", request.params.email);
+
   response.success();
 });
