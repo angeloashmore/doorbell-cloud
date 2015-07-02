374609a2db083c68c656c5594e53207c36e1d936
Angelo Ashmore <angeloashmore@me.com>
2015-07-01 17:20:51 -1000

    Fixing bug with setting username as email automatically

--- cloud/functions/_User/_beforeSave.js
+++ cloud/functions/_User/_beforeSave.js
@@ -3,7 +3,7 @@ const User = require("cloud/classes/_User");
 Parse.Cloud.beforeSave(User, function(request, response) {
   const user = request.object;
 
-  user.set("username", request.params.email);
+  user.set("username", user.email);
 
   response.success();
 });
